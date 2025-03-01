c73b674a2a578a4f7a933b0675eed73f63353b7b
markderrck5445 <nguyagwamarkderrick@gmail.com>
2025-03-01 20:12:10 +0300

    added new file

---
+++ script.js
@@ -0,0 +1,59 @@
+const toggleSwitch=document.getElementById('checkbox');
+const currentTheme=localStorage.getItem('theme');
+console.log('Current themefrom localStorage:',currentTheme);
+
+if(currentTheme){
+    document.body.classList.toggle('dark-mode',currentTheme==='dark');
+    toggleSwitch.checked=currentTheme==='dark';
+    console.log('Dark mode set:', currentTheme==='dark');
+}
+
+toggleSwitch.addEventListener('change',function(){
+    if(toggleSwitch.checked) {
+        document.body.classList.add('dark-mode');
+        localStorage.setItem('theme','dark');
+        console.log('dark=mode enabled');
+    }else{
+        document.body.classList.remove('dark-mode');
+        localStorage.setItem('theme','light');
+        console.log('dark-mode disabled');
+    }
+});
+
+function button(buttonId){
+    const button = document.getElementById(buttonId);
+    button.innerText = 'BOOKED';
+    button.classList.add('booked');
+    button.disabled = true;
+    
+    alert('your booking is underway.Wait for your confirmation.');
+}
+
+document.getElementById('button').addEventListener('click',function(){
+    button('button');
+});
+document.getElementById('button1').addEventListener('click',function(){
+    button('button1');
+});
+document.getElementById('button2').addEventListener('click',function(){
+    button('button2');
+});
+document.getElementById('button3').addEventListener('click',function(){
+    button('button3');
+});
+document.getElementById('button4').addEventListener('click',function(){
+    button('button4');
+});
+document.getElementById('button5').addEventListener('click',function(){
+    button('button5');
+});
+document.getElementById('button6').addEventListener('click',function(){
+    button('button6');
+});
+document.getElementById('button7').addEventListener('click',function(){
+    button('button7');
+});
+document.getElementById('button8').addEventListener('click',function(){
+    button('button8');
+});
+
